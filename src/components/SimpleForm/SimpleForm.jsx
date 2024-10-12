cd4856f90967e55d9084d1c3e93ca8783968f291
ranamiah221 <ranarasul21@gmail.com>
2024-10-12 22:33:29 +0600

    create a form and added a submit button for this

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,9 +1,18 @@
 
 
 const SimpleForm = () => {
+    const handleSubmit=e=>{
+        e.preventDefault();
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+        console.log(e.target.phone.value);
+        console.log("submit btn clicked");
+    }
     return (
-       <form>
-             <input type="text" name="name"/><br />
+       <form onSubmit={handleSubmit}>
+             <input type="text" name="name"/> <br />
+             <input type="email" name="email"/> <br />
+             <input type="text" name="phone" /> <br />
              <input type="submit" value="Submit" />
        </form>
     );

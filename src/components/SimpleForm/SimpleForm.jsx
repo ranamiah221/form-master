eaf84d7f386c71db43514af7e69af0c31b2afd34
ranamiah221 <ranarasul21@gmail.com>
2024-10-12 22:25:13 +0600

    create a repo for react router core concepts part 3 and also work in form

---
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,12 @@
+
+
+const SimpleForm = () => {
+    return (
+       <form>
+             <input type="text" name="name"/><br />
+             <input type="submit" value="Submit" />
+       </form>
+    );
+};
+
+export default SimpleForm;

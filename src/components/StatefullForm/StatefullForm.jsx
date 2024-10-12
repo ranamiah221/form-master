65e5cbcf6f10e3caaea89fea6fdce9bdc7f13fa4
ranamiah221 <ranarasul21@gmail.com>
2024-10-12 23:07:40 +0600

    added a use state full form

---
+++ src/components/StatefullForm/StatefullForm.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+
+const StatefullForm = () => {
+    const [name,setName]=useState("rana");
+    const [email,setEmail]=useState(null);
+    const [password, setPassword]=useState(null);
+    const [error,setError]=useState('')
+
+    const handleSubmit=e=>{
+        e.preventDefault();
+        if(password.length < 6){
+            setError('Password length must be 6 or more character')
+        }
+        else{
+             setError('')
+            console.log(name,email, password)
+        }
+        
+       }
+    const handleNameChange=e=>{
+        setName(e.target.value);
+    }
+    const handleEmailChange=e=>{
+        setEmail(e.target.value);
+    }
+    const handlePasswordChange=e=>{
+        setPassword(e.target.value);
+    }
+    return (
+        <div>
+             <form onSubmit={handleSubmit}>
+                   <input value={name}
+                   onChange={handleNameChange}
+                   type="text" name="name"/> <br />
+
+                   <input 
+                   onChange={handleEmailChange}
+                   type="email" name="email"/> <br />
+                   <input 
+                   onChange={handlePasswordChange}
+                   type="password" name="password" required/> <br />
+                   <input type="submit" value="Submit" />
+                   {
+                    error && <p>{error}</p>
+                   }
+             </form>
+        </div>
+    );
+};
+
+export default StatefullForm;
